77225c5b74baff5c345489cb8698b76f49d56b21
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-22 13:22:04 -0800

    implemented create route

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
 
+app.get("/comments/new", (req, res) => {
+  res.render("comments/new");
+});
+
+app.post("/comments/new", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment });
+  res.send("IT WORKED");
+});
+
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos response");
 });
