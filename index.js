1ebe3fa928eab6483667ff5818520ee2485ed51d
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-22 12:14:48 -0800

    Created comment index for the read part of CRUD

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+const comments = [
+  {
+    username: "Todd",
+    comment: "lol that is so funny",
+  },
+  {
+    username: "Skyler",
+    comment: "I like bird watching with my dog",
+  },
+  {
+    username: "Sk8erBoi",
+    comment: "Plz delete your account Todd",
+  },
+  {
+    username: "onlysayswoof",
+    comment: "woof woof woof",
+  },
+  {
+    username: "Jeff",
+    comment: "none of you really exist",
+  },
+];
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", { comments });
+});
 
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos response");
