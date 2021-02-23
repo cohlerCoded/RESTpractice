d7fb7a2da0aa25a08bfc0e176922719a5a2dbaeb
cohlerCoded <jeffcohler@cohlercoded.com>
2021-02-23 10:25:34 -0800

    made an edit form, implemented method override

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const { v4: uuid } = require("uuid");
 const app = express();
 const path = require("path");
+const methodOverride = require("method-override");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
+//fake db
 const comments = [
   {
     id: uuid(),
@@ -35,33 +37,39 @@ const comments = [
     comment: "none of you really exist",
   },
 ];
-
+//see all comments
 app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
-
+//new comment form
 app.get("/comments/new", (req, res) => {
   res.render("comments/new");
 });
-
+//make a new comment
 app.post("/comments/new", (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment, id: uuid() });
   res.redirect("/comments");
 });
-
+//get specific comment
 app.get("/comments/:id", (req, res) => {
   const { id } = req.params;
   const comment = comments.find((comment) => comment.id === id);
   res.render("comments/show", { comment });
 });
-
+//edit backend functionality
 app.patch("/comments/:id", (req, res) => {
   const { id } = req.params;
   const newComment = req.body.comment;
   const oldComment = comments.find((comment) => comment.id === id);
   oldComment.comment = newComment;
   res.redirect("/comments");
+});
+//edit form
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((comment) => comment.id === id);
+  res.render("comments/edit", { comment });
 });
 
 app.get("/tacos", (req, res) => {
